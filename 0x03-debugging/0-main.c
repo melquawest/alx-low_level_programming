f66051f75eaa500ba381ae878847d98b3e60e4a7
melquawest <pboakye118@gmail.com>
2023-03-24 21:17:08 -0700

    C debugging

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+* main - This is going to test the function for
+* both positive and negative integers.
+* Return: 0
+*/
+int main(void)
+{
+int i;
+i = 0;
+positive_or_negative(i);
+return (0);
+}
---
+++ 0x03-debugging/REAMDME.md
@@ -0,0 +1,1 @@
+This directory throws light on DEBUGGING a code
