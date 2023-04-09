78e7a64658820e8e627066cb89cf25579f70fe59
melquawest <pboakye118@gmail.com>
2023-04-09 06:06:33 -0700

    Even more pointers, arrays and strings in C

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -8,11 +8,13 @@
  */
 char *_strchr(char *s, char c)
 {
-while (*s != '\0') {
-if (*s == c) {
-return s;
+while (*s != '\0')
+{
+if (*s == c)
+{
+return (s);
 }
 s++;
 }
-return (*s == c) ? s : NULL;
+return ((*s == c) ? s : NULL);
 }
